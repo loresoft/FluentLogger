d488f9f8e19afd0139de912681ca492e5deba08a
Paul Welter <pwelter@loresoft.com>
2016-07-29 12:31:48 -0500

    convert to .net core

---
+++ src/FluentLogger/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FluentLogger")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("LoreSoft")]
+[assembly: AssemblyProduct("FluentLogger")]
+[assembly: AssemblyCopyright("Copyright © LoreSoft 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
